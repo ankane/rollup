21d68a5095d7d88fbee2fd4c5cbcc5b9aa0b6b69
Andrew Kane <andrew@ankane.org>
2023-06-05 00:24:12 -0700

    Switched to require_relative

--- lib/rollups.rb
+++ lib/rollups.rb
@@ -4,14 +4,14 @@ require "groupdate"
 
 ActiveSupport.on_load(:active_record) do
   # must come first
-  require "rollup"
+  require_relative "rollup"
 
-  require "rollup/model"
+  require_relative "rollup/model"
   extend Rollup::Model
   Rollup.rollup_column = :time
 
   # modules
-  require "rollup/aggregator"
-  require "rollup/utils"
-  require "rollup/version"
+  require_relative "rollup/aggregator"
+  require_relative "rollup/utils"
+  require_relative "rollup/version"
 end
